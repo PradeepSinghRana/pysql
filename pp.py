6ed57cddfeb8eb6adaac5595a46ef5ed21303b75
pradeep <pradeeprana432@gmail.com>
2019-02-19 10:03:45 +0530

    xaddad

---
+++ pp.py
@@ -0,0 +1,13 @@
+#name=str(input("enter name:"))
+#gender=str(input("enter gender" ))
+#age=int(input("enter age:"))
+#print(name,"is a good",gender,"and his age",age)
+#print("RAM"+" is a good"+" boy"+" and his age"+str(14))
+#print("%s is a good %s and his age is %d" %(name,gender,age))
+#print("%s is a good %s and his age is %d" %(ram,boy,14))
+
+print("%s whose Roll no. is %d has got %.2f in last semester"%("Pradeep",1,99.99))
+
+print("%s has a rating of %.2f on %s"%("Gully",9.10,"IMDB"))
+
+print("THe value of %s is %f"%("pi",3.14))
